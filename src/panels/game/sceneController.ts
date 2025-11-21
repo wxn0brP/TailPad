9bacd0818e877959970a8e910ed04ec2a8bd7f31
wxn0brP <toja.filiper@gmail.com>
2025-11-21 13:30:58 +0100

    feat: action emitter

--- src/panels/game/sceneController.ts
+++ src/panels/game/sceneController.ts
@@ -1,6 +1,9 @@
+import VEE from "@wxn0brp/event-emitter";
 import { selectPrompt, uiMsg } from "@wxn0brp/flanker-dialog";
 import { GameScene } from "./state";
-import { Action, ActionBase } from "./types";
+import { Action, ActionBackgroundType, ActionDelayType, ActionDialogChoiceType, ActionGoToSceneType, ActionTextType } from "./types";
+
+export const actionEmitter = new VEE();
 
 export function sceneController(scene: GameScene) {
     scene.eventEmitter.on("run-scene", (index: number) => {
@@ -9,37 +12,46 @@ export function sceneController(scene: GameScene) {
     });
 }
 
-async function waitForEnd(action: ActionBase, cb: (...args: any[]) => Promise<any>, ...args: any[]) {
-    const promise = cb(...args);
-    if (!action.noWaitForEnd) await promise;
-    return Promise.resolve();
-}
-
 async function runScene(scene: GameScene, action: Action, index: number) {
     if (!action) return console.error("No action", index, scene);
-    switch (action.type) {
-        case "text":
-            await waitForEnd(action, () => scene.dialogEngine.write(action.text));
-            break;
-        case "background":
-            scene.setBackground(action.url);
-            break;
-        case "delay":
-            await waitForEnd(action, () => new Promise((resolve) => setTimeout(resolve, action.ms)));
-            break;
-        case "go-to-scene":
-            uiMsg("Go to scene: " + action.scene);
-            break;
-        case "dialog-choice":
-            const sceneName = await selectPrompt(action.text, action.choices.map(c => c.text), action.choices.map(c => c.scene));
-            uiMsg("Go to scene: " + sceneName);
-            break;
-        default:
-            const n: never = action;
-            throw new Error(`Unknown action type: ${n}`);
+
+    if (action.noWaitForEnd) {
+        actionEmitter.emit(action.type, () => { }, action, scene, index);
+    } else {
+        await new Promise((resolve) => {
+            actionEmitter.emit(action.type, resolve, action, scene, index);
+        });
     }
 
     if (index === scene.sceneConfig.length - 1) return scene.eventEmitter.emit("scenes-end");
     if (scene.pause.get()) return scene.eventEmitter.emit("pause", index);
     scene.eventEmitter.emit("run-scene", index + 1);
 }
+
+function register() {
+    actionEmitter.on("text", async (cb, action: ActionTextType, scene: GameScene) => {
+        scene.dialogEngine.write(action.text).then(cb);
+    });
+
+    actionEmitter.on("background", async (cb, action: ActionBackgroundType, scene: GameScene) => {
+        scene.setBackground(action.url);
+        cb();
+    });
+
+    actionEmitter.on("delay", async (cb, action: ActionDelayType) => {
+        setTimeout(cb, action.ms);
+    });
+
+    actionEmitter.on("go-to-scene", async (cb, action: ActionGoToSceneType) => {
+        uiMsg("Go to scene: " + action.scene);
+        cb();
+    });
+
+    actionEmitter.on("dialog-choice", async (cb, action: ActionDialogChoiceType) => {
+        const sceneName = await selectPrompt(action.text, action.choices.map(c => c.text), action.choices.map(c => c.scene));
+        uiMsg("Go to scene: " + sceneName);
+        cb();
+    });
+}
+
+register();
